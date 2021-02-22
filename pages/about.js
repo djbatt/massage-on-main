d36575619365baf090282778a81d33f3356692a6
djbatt <contact@davidbattiston.com>
2021-02-21 20:08:33 -0500

    added DatoCMS

---
+++ pages/about.js
@@ -0,0 +1,36 @@
+import { request } from "../lib/datocms";
+import parse from "html-react-parser";
+
+const ABOUT_QUERY = `query {
+  page(filter: {id: {eq: "21725847"}}) {
+    bannerText
+    id
+    pageTitle
+    summary(markdown: true)
+  }
+}`;
+
+function Page({ data }) {
+  // Render data...
+  return (
+    <div>
+      <h1>{data.page.bannerText}</h1>
+      {parse(data.page.summary)}
+    </div>
+  );
+}
+
+// This gets called on every request
+export async function getServerSideProps() {
+  const data = await request({
+    query: ABOUT_QUERY,
+    variables: { limit: 10 },
+  });
+
+  console.log(data);
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
+
+export default Page;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,51 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { request } from "../lib/datocms";
+import { Image } from "react-datocms";
+import Head from "next/head";
 
-export default function Home() {
+const HOMEPAGE_QUERY = `query {
+  page(filter: {id: {eq: "21725734"}}) {
+    bannerText
+    id
+    pageTitle
+    summary(markdown: true)
+    banner {
+      responsiveImage {
+        alt
+        src
+        srcSet
+        base64
+      }
+    }
+  }
+}
+`;
+
+function Page({ data }) {
+  // Render data...
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Massage On Main</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Image data={data.page.banner.responsiveImage} />
+      <h1>{data.page.bannerText}</h1>
     </div>
-  )
+  );
 }
+
+// This gets called on every request
+export async function getServerSideProps() {
+  const data = await request({
+    query: HOMEPAGE_QUERY,
+    variables: { limit: 10 },
+  });
+
+  console.log(data, "data");
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
+
+export default Page;
